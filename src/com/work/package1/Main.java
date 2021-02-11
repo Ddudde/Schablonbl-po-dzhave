a50e003b3d0775db77ca410a41f5552dca4c686e
Dude <BAX.152MM@yandex.ru>
2021-02-11 15:42:02 +0300

    Prakt#1v1

--- src/com/work/package1/Main.java
+++ src/com/work/package1/Main.java
@@ -1,13 +1,29 @@
 package com.work.package1;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.util.function.Function;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Function<int[], Integer> abrakadabra = (x) -> {
             return gcd(x[0],x[1]);
         };
-        System.out.println(abrakadabra.apply(new int[]{6, 3}));
+        Scanner in = new Scanner(System.in);
+        int num1 = 0, num2 = 0;
+        try {
+            System.out.print("Ваше число: ");
+            num1 = in.nextInt();
+            System.out.print("Ваше число: ");
+            num2 = in.nextInt();
+        }
+        catch (InputMismatchException e)
+        {
+            System.out.println("Введены некорректные данные!");
+            return;
+        }
+        System.out.println("НОД: " + abrakadabra.apply(new int[]{num1, num2}));
     }
     private static int gcd(int a, int b) {
         if (b==0) return a;
